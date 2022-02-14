6c0213153c7a60c06a5eb6031d76d31ca46a601e
ABMint <abilashsenth@gmail.com>
2022-02-14 11:18:19 +0530

    secrets lvl

--- Secrets-Webapp/app.js
+++ Secrets-Webapp/app.js
@@ -1,10 +1,65 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const mongoose = require("mongoose");
+const mongooseEncryption = require("mongoose-encryption");
 
 const app = express();
 
 app.use(express.static("public"));
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
+app.use(bodyParser.urlencoded({ extended: true }));
+mongoose.connect("mongodb://localhost:27017/userDB", { useNewUrlParser: true });
 
+const userSchema = new mongoose.Schema({
+  email: String,
+  password: String,
+});
+
+const secret = "Thisisourlittlesecret.";
+userSchema.plugin(encrypt, { secret: secret, encryptedFields: ["password"] });
+
+const User = mongoose.model("User", userSchema);
+
+app.get("/", function (req, res) {
+  res.render("home");
+});
+
+app.get("/login", function (req, res) {
+  res.render("login");
+});
+
+app.get("/register", function (req, res) {
+  res.render("register");
+});
+
+app.post("/login", function (req, res) {
+  const userName = req.body.username;
+  const userPassword = req.body.password;
+  User.findOne({ email: userName }, function (err, foundUser) {
+    if (err) {
+      console.log(err);
+    } else if (foundUser.password === userPassword) {
+      res.render("secrets");
+    }
+  });
+});
+
+app.post("/register", function (req, res) {
+  const user = new User({
+    email: req.body.username,
+    password: req.body.password,
+  });
+  user.save(function (err) {
+    if (!err) {
+      res.render("secrets");
+    } else {
+      console.log(err);
+    }
+  });
+});
+
+app.listen(3000, function () {
+  console.log("listening on port 3000");
+});
