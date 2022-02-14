bcf391ca1e6d3e0f1fa5d5a4829ab7da7674143c
abilashsenth <abilashsenth@gmail.com>
2022-02-14 13:03:31 +0530

    add env files

--- Secrets-Webapp/app.js
+++ Secrets-Webapp/app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
 
 const app = express();
 
@@ -19,8 +19,6 @@ const userSchema = new mongoose.Schema({
   email: String,
   password: String,
 });
-
-userSchema.plugin(encrypt, { secret: secret, encryptedFields: ["password"] });
 
 const User = mongoose.model("User", userSchema);
 
@@ -42,7 +40,7 @@ app.get("/register", function (req, res) {
 
 app.post("/login", function (req, res) {
   const userName = req.body.username;
-  const userPassword = req.body.password;
+  const userPassword = md5(req.body.password);
   User.findOne({ email: userName }, function (err, foundUser) {
     if (err) {
       console.log(err);
@@ -55,7 +53,7 @@ app.post("/login", function (req, res) {
 app.post("/register", function (req, res) {
   const user = new User({
     email: req.body.username,
-    password: req.body.password,
+    password: md5(req.body.password),
   });
   user.save(function (err) {
     if (!err) {
