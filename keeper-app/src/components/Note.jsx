63a7c20b6e833094fc66edf169b7a83d1c162f7d
abilashsenth <abilashsenth@gmail.com>
2022-02-25 00:50:54 +0530

    practice

---
+++ keeper-app/src/components/Note.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+function Note(){
+    return (
+        <div className="note">
+            <h1>Note Title</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quidem.</p>
+        </div>
+    )
+}
+
+export default Note;
